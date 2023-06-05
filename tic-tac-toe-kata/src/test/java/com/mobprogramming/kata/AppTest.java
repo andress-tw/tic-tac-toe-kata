314eef327ba9aa253b9a240ea317d4a4c4778e86
Andres Salazar <ssalazar@thoughtworks.com>
2023-06-05 11:35:58 +0100

    andres: testing app setup

--- tic-tac-toe-kata/src/test/java/com/mobprogramming/kata/AppTest.java
+++ tic-tac-toe-kata/src/test/java/com/mobprogramming/kata/AppTest.java
@@ -1,8 +1,7 @@
 package com.mobprogramming.kata;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
 
 /**
  * Unit test for simple App.
@@ -10,29 +9,15 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+    private static final String INPUT = "123456";
+
+    public void testLength() {
+        Assert.assertEquals(64, App.sha256hex(INPUT).length());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testHex() {
+        String expected = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
+        Assert.assertEquals(expected, App.sha256hex(INPUT));
     }
 }
